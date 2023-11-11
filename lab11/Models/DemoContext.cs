76a498b9d03b3d8a22deb7fc49a76ef11159ff95
VictorOrrillo <victor.orillo@tecsup.edu.pe>
2023-11-10 21:22:21 -0500

    Agregar archivos de proyecto.

---
+++ lab11/Models/DemoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace lab11.Models
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options): base(options) 
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Detail> Details { get; set; }
+
+    }
+}
